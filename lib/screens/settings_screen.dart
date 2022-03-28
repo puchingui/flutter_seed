8d1e96a949720d35d235ca1029cbd40dc24197e4
Kenneth Burgos <kenneth.burgos@gmail.com>
2022-03-28 16:13:50 -0400

    On SettingsScreen no listen for ThemeProvider

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -19,7 +19,7 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
+    final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
 
     return Scaffold(
         appBar: AppBar(title: const Text('SettingsScreen')),
